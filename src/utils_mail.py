94fb2066ac02fb6c2e03f1c98db54be8fa57909d
THINKPADX1-JO\johan <johan.tre@gmail.com>
2025-03-30 20:20:36 +0200

    Fixed long absolute path in mail

--- src/utils_mail.py
+++ src/utils_mail.py
@@ -24,7 +24,7 @@ def send_email(generated_files, email_address, provider_replacements, customer_r
     klant_naam = safe_get(customer_replacements, "KlantNaam")
     klant_job_title = safe_get(customer_replacements, "KlantJobTitle")
     klant_job_reference = safe_get(customer_replacements, "KlantJobReference")
-    base_document = f"{config["paths"]["base_output_document_path"]} - {leverancier_naam} - {klant_naam} - {klant_job_title} - {klant_job_reference}"
+    base_document = f"{config["paths"]["base_document_name"]} - {leverancier_naam} - {klant_naam} - {klant_job_title} - {klant_job_reference}"
 
     email_subject = f"Recht om te vertegenwoordigen documents for '{klant_naam}' for '{klant_job_title}' ({klant_job_reference})"
     email_message = EmailMessage()
