3248adfe1240716d0635aef0dbc0688ecf179922
THINKPADX1-JO\johan <johan.tre@gmail.com>
2025-04-05 17:17:23 +0200

    forgotten Git commit push added

--- src/msword_properties_generator/main/remove_provider.py
+++ src/msword_properties_generator/main/remove_provider.py
@@ -1,11 +1,11 @@
 from msword_properties_generator.data.utils_db import init_db, remove_provider
-from msword_properties_generator.utils.utils_image import remove_from_image_folder
+from msword_properties_generator.utils.utils_image import remove_from_image_folder_git_commit_push
 
 
 conn = init_db()
 
 remove_provider(conn)
-remove_from_image_folder()
+remove_from_image_folder_git_commit_push()
 
 # Commit and close
 conn.commit()
