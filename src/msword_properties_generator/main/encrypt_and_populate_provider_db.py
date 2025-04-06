109af49fbc56b7a76d342f2ca8e481dfcceca8f6
THINKPADX1-JO\johan <johan.tre@gmail.com>
2025-04-06 15:02:30 +0200

    Omitted double install dependencies + catch error when bad image = no change committed to repo.

--- src/msword_properties_generator/main/encrypt_and_populate_provider_db.py
+++ src/msword_properties_generator/main/encrypt_and_populate_provider_db.py
@@ -1,19 +1,28 @@
 from msword_properties_generator.data.utils_db import init_db, create_table_if_not_exist, get_inputs_and_encrypt, insert_or_update_into_db
 from msword_properties_generator.utils.utils_image import get_image_and_encrypt_to_image_folder
 from msword_properties_generator.utils.utils_logging import setup_logging
+import logging
 
 
 
 setup_logging()
 conn = init_db()
-create_table_if_not_exist(conn)
 
-encrypted_inputs = get_inputs_and_encrypt()
+try:
+    create_table_if_not_exist(conn)
 
-get_image_and_encrypt_to_image_folder()
+    encrypted_inputs = get_inputs_and_encrypt()
 
-insert_or_update_into_db(conn, encrypted_inputs)
+    get_image_and_encrypt_to_image_folder()
 
-# Commit and close
-conn.commit()
-conn.close()
+    insert_or_update_into_db(conn, encrypted_inputs)
+
+    # Commit and close
+    conn.commit()
+except SystemExit as e:
+    if e.code != 0:
+        logging.error("An error occurred, exiting.")
+    else:
+        raise  # Re-raise the exception if it's a normal exit
+finally:
+    conn.close()
