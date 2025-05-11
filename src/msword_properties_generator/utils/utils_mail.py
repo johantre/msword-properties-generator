f2b6cb5b8f899cf84a8f5fb13770079ba9076cdb
THINKPADX1-JO\johan <johan.tre@gmail.com>
2025-05-11 19:07:40 +0200

    info@ isn't supported by gravatar

--- src/msword_properties_generator/utils/utils_mail.py
+++ src/msword_properties_generator/utils/utils_mail.py
@@ -29,9 +29,7 @@ def send_email(generated_files, email_address, provider_replacements, customer_r
     email_subject = f"Recht om te vertegenwoordigen documents for '{klant_naam}' for '{klant_job_title}' ({klant_job_reference})"
     email_message = EmailMessage()
     email_message['Subject'] = email_subject
-    email_message['From'] = formataddr(("Github Actions", config['mail']['mail_sender_email']))
-    email_message['Organization-Name'] = 'Dreamlead'
-    email_message['Organization-Logo-URL'] = 'https://bimi.dreamlead.be/logo-dreamlead.svg'
+    email_message['From'] = formataddr(("Dreamlead Github Actions", config['mail']['mail_sender_email']))
     email_message['To'] = email_address
     email_message.set_content(return_html_body(base_document, leverancier_naam, klant_naam, klant_job_title, klant_job_reference), subtype='html')
 
