ccc5bcac135fc11476031be850fbd6d3082d3133
THINKPADX1-JO\johan <johan.tre@gmail.com>
2025-05-11 11:44:14 +0200

    BIMI compliant logo

--- src/msword_properties_generator/utils/utils_mail.py
+++ src/msword_properties_generator/utils/utils_mail.py
@@ -30,6 +30,8 @@ def send_email(generated_files, email_address, provider_replacements, customer_r
     email_message = EmailMessage()
     email_message['Subject'] = email_subject
     email_message['From'] = formataddr(("Github Actions", config['mail']['mail_sender_email']))
+    email_message['Organization-Name'] = 'Dreamlead'
+    email_message['Organization-Logo-URL'] = 'https://bimi.dreamlead.be/logo-dreamlead.svg'
     email_message['To'] = email_address
     email_message.set_content(return_html_body(base_document, leverancier_naam, klant_naam, klant_job_title, klant_job_reference), subtype='html')
 
