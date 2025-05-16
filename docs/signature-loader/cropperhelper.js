b1ad466b5891932efbf952f0b54905e5f0fc865f
THINKPADX1-JO\johan <johan.tre@gmail.com>
2025-05-16 20:25:44 +0200

    remove drag mode (esp for mobile)

--- docs/signature-loader/cropperhelper.js
+++ docs/signature-loader/cropperhelper.js
@@ -8,7 +8,7 @@ let originalWidth = 0;
 let originalHeight = 0;
 let currentWidth = 0;
 let currentHeight = 0;
-let LOGGING_ENABLED = false;
+let LOGGING_ENABLED = true;
 
 // Cropper and image state
 const preview = document.getElementById('preview');
